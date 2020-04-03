b7abe0d8cc156aac49bb33b0eb495e00d7eae97c
Furkan Ünsal <36279100+unsalfurkanali@users.noreply.github.com>
2020-04-03 20:38:10 +0300

    Delete q2.py

--- q2.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Please enter a number"))
-divider = 1
-for i in range(2, x):
-    if not (x%i):
-        divider = divider + i
-
-if divider == x:
-    print("{} is a perfect number!".format(x))
